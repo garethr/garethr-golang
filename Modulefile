6beb1d967be8e4503ea02612acc9b0e267d68765
Gareth Rushgrove <gareth@morethanseven.net>
2013-04-13 18:23:28 +0100

    pin dependencies, to test automatic publishing

--- Modulefile
+++ Modulefile
@@ -1,9 +1,10 @@
 name        'garethr-golang'
-version     '0.0.3'
+version     '0.0.4'
 source      'git://github.com/garethr/garethr-golang.git'
 author      'Gareth Rushgrove'
 summary     'Module for installing go from official ppa'
 description 'Install one of the official golang packages'
 license     'Apache License, Version 2.0'
-dependency  'puppetlabs/apt'
-dependency  'puppetlabs/stdlib'
+
+dependency  'puppetlabs/apt', '>= 1.1.0'
+dependency  'puppetlabs/stdlib', '>=3.2.0'
--- spec/classes/golang_spec.rb
+++ spec/classes/golang_spec.rb
@@ -4,6 +4,7 @@ describe 'golang', :type => :class do
   let(:facts) { {:osfamily => 'Debian', :lsbdistcodename => 'precise'} }
 
   context 'with no parameters' do
+    it { should include_class('apt') }
     it { should contain_package('new-golang').with_name('golang-stable').with_ensure('present') }
     it { should contain_package('system-golang').with_name('golang').with_ensure('absent') }
     it { should contain_apt__ppa('ppa:gophers/go') }
@@ -18,6 +19,7 @@ describe 'golang', :type => :class do
     let(:params) { {'package_name' => 'golang-tip' } }
     it { should contain_package('new-golang').with_name('golang-tip').with_ensure('present') }
   end
+
   context 'with a custom version' do
     let(:params) { {'version' => 'absent' } }
     it { should contain_package('new-golang').with_ensure('absent') }
@@ -33,13 +35,12 @@ describe 'golang', :type => :class do
   end
 
   context 'with an invalid distro name' do
-    let(:facts) { {:osfamily => 'RedHat', :lsbdistcodename => 'centos'} }
+    let(:facts) { {:osfamily => 'RedHat'} }
     it do
       expect {
         should contain_package('new-golang')
       }.to raise_error(Puppet::Error, /This module uses PPA repos/)
     end
   end
-
 
 end
